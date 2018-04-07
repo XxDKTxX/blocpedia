c6e179fbdec397d4b327bfcd2ee2cc2adbf4a755
Devin Tassie <devintassie@gmail.com>
2018-04-07 01:53:32 +0000

    migration complete. devise configured and working

--- db/migrate/20180407005614_add_devise_to_users.rb
+++ db/migrate/20180407005614_add_devise_to_users.rb
@@ -2,9 +2,9 @@
 
 class AddDeviseToUsers < ActiveRecord::Migration[5.1]
   def self.up
-    change_table :users do |t|
+    create_table :users do |t|
       ## Database authenticatable
-      t.change :email, :string,     null: false, default: ""
+      t.string :email,     null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
@@ -37,7 +37,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.1]
       # t.timestamps null: false
     end
 
-    # add_index :users, :email,                unique: true
+    add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
